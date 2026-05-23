1e11bf1bd61e43eb620d94d51aade4897a31b458
Louie Lee <louie@RubbishMBP.local>
2026-05-23 22:22:28 +0800

    ref: remove deprecated which key settings

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,18 +5,16 @@ return {
 	event = "VeryLazy",
 	opts = {
 		spec = {
-			{ "<leader>a", group = "+[A]vante", mode = modes, icon = "" },
 			{ "<leader>b", group = "+[B]uffer", mode = modes },
-			{ "<leader>C", group = "+[C]opilot Chat", mode = modes, icon = "" },
-			{ "<leader>c", group = "+[C]ode/ [Copilot]", mode = modes, icon = "" },
-			{ "<leader>d", group = "+[D]ocument/ [D]iffvew", mode = modes },
+			{ "<leader>c", group = "+[C]ode", mode = modes },
+			{ "<leader>d", group = "+[D]ocument/ [D]iffview", mode = modes },
+			{ "<leader>g", group = "+[G]itsigns", mode = modes },
 			{ "<leader>r", group = "+[R]ename ", mode = modes },
 			{ "<leader>w", group = "+[W]orkspace", mode = modes },
 			{ "<leader>x", group = "+Trouble [X]", mode = modes },
 			{ "<leader>p", group = "+[P]rint", mode = modes },
 			{ "<leader>s", group = "+[S]earch", mode = modes },
 			{ "<leader>K", group = "+[C]ase Change", mode = modes },
-			{ "<leader>g", group = "+[G]ptPrompt", mode = modes, icon = "" },
 		},
 	},
 	keys = {
