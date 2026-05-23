cf4c36491bcf924d58e58be7e8df0908057d7510
Louie Lee <louie@RubbishMBP.local>
2026-05-23 23:11:37 +0800

    feat: track explorer hidden state

--- .config/nvim/lua/plugins/snacks/init.lua
+++ .config/nvim/lua/plugins/snacks/init.lua
@@ -1,5 +1,38 @@
 local dashboard = require("plugins.snacks.dashboard")
 local explorer = require("plugins.snacks.explorer")
+local explorer_hidden = false
+
+local function current_explorer()
+  return Snacks.picker.get({ source = "explorer" })[1]
+end
+
+local function explorer_opts(opts)
+  local current = current_explorer()
+  if current then
+    explorer_hidden = current.opts.hidden
+  end
+  opts = vim.tbl_extend("force", { hidden = explorer_hidden }, opts or {})
+  local on_close = opts.on_close
+  opts.on_close = function(picker)
+    explorer_hidden = picker.opts.hidden
+    if on_close then
+      on_close(picker)
+    end
+  end
+  return opts
+end
+
+local function reveal_explorer()
+  if current_explorer() then
+    Snacks.explorer.reveal()
+  else
+    Snacks.explorer(explorer_opts({
+      on_show = function()
+        Snacks.explorer.reveal()
+      end,
+    }))
+  end
+end
 
 return {
   "folke/snacks.nvim",
@@ -29,7 +62,7 @@ return {
     lazygit = { enabled = true },
   },
   keys = {
-    { "<leader>e", function() Snacks.explorer() end, desc = "[E]xplore Snacks Explorer" },
-    { "<leader>E", function() Snacks.explorer.reveal() end, desc = "[E]xplore Current File" },
+    { "<leader>e", function() Snacks.explorer(explorer_opts()) end, desc = "[E]xplore Snacks Explorer" },
+    { "<leader>E", reveal_explorer, desc = "[E]xplore Current File" },
   },
 }
